100cf189e288b6d66358e6ab93a6324a7134af82
ilkinsdqv <97682974+ilkinsdqv@users.noreply.github.com>
2023-02-20 21:16:36 +0400

    Project Start

---
+++ bookShoppingProject/src/main/java/bookShopping/bookShoppingProject/BookShoppingProjectApplication.java
@@ -0,0 +1,13 @@
+package bookShopping.bookShoppingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShoppingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShoppingProjectApplication.class, args);
+	}
+
+}
---
+++ bookShoppingProject/src/test/java/bookShopping/bookShoppingProject/BookShoppingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package bookShopping.bookShoppingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShoppingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
